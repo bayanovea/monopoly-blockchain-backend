7f1ecc8602768c514093604c4ee52f94f4280033
evgeniy.bayanov <e.bayanov@skyeng.ru>
2022-04-20 01:39:00 +0700

    Add dotenv and use env for blockchain connection

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,13 @@
+require("dotenv").config()
+
 module.exports = {
+  networks: {
+    development: {
+      host: process.env.HOST,
+      port: process.env.PORT,
+      network_id: process.env.NETWORK_ID
+    }
+  },
   compilers: {
     solc: {
       version: "^0.8.0"
